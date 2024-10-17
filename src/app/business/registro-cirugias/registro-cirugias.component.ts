8008779eb8352df34a12f887b8a91c73ca950399
KikeRO03 <152652282+KikeRO03@users.noreply.github.com>
2024-10-17 08:49:16 -0600

    Logro: Se genera el componente registro-cirugias en business

---
+++ src/app/business/registro-cirugias/registro-cirugias.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registro-cirugias',
+  standalone: true,
+  imports: [],
+  templateUrl: './registro-cirugias.component.html',
+  styleUrl: './registro-cirugias.component.css'
+})
+export class RegistroCirugiasComponent {
+
+}
